b74d2a9ba4efb82f0a7cbe129e663ad929282702
meeraraveendran006 <meeraraveendran006@gmail.com>
2026-08-07 10:55:07 +0530

    Time: 4 ms (92.65%), Space: 44.2 MB (80.07%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,36 +1,42 @@
 class Solution {
-    // Function to reverse the order of words in a string
+    // Function to reverse the order of words 
     public String reverseWords(String s) {
-        // List to store words
-        List<String> words = new ArrayList<>();
+        // StringBuilder for final result
+        StringBuilder result = new StringBuilder();
         
-        // StringBuilder to store a single word
-        StringBuilder word = new StringBuilder();
+        // Pointer starting from the end
+        int i = s.length() - 1;
         
-        // Traverse each character in the string
-        for (int i = 0; i < s.length(); i++) {
-            // If it's not a space, add it to current word
-            if (s.charAt(i) != ' ') {
-                word.append(s.charAt(i));
-            } 
-            // If space and a word is formed
-            else if (word.length() > 0) {
-                // Add word to list
-                words.add(word.toString());
-                // Reset word
-                word.setLength(0);
+        // Traverse from right to left
+        while (i >= 0) {
+            // Skip spaces
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
             }
+            
+            // If pointer goes out of bounds, break
+            if (i < 0) break;
+            
+            // Mark end of word
+            int end = i;
+            
+            // Move left until space or start of string
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            
+            // Extract the word
+            String word = s.substring(i + 1, end + 1);
+            
+            // Add space before appending if result is not empty
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+            
+            // Append word
+            result.append(word);
         }
         
-        // Add the last word if present
-        if (word.length() > 0) {
-            words.add(word.toString());
-        }
-        
-        // Reverse the list
-        Collections.reverse(words);
-        
-        // Join with single space
-        return String.join(" ", words);
+        return result.toString();
     }
 }
